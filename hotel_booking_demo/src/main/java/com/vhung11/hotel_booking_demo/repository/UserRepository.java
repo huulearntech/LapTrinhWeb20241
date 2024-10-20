833b000cde6fa9723dc3b7bcc7909237b1120631
Bui Van Huy <98834617+buiihuy@users.noreply.github.com>
2024-10-20 16:09:23 +0700

    Delete demo directory

--- hotel_booking_demo/src/main/java/com/vhung11/hotel_booking_demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vhung11.hotel_booking_demo.repository;
-
-import com.vhung11.hotel_booking_demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
