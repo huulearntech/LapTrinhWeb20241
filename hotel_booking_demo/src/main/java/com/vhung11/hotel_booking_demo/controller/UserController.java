661cf97d0f1c2f761a9c64e059be5d35de165417
hung <admin@LAPTOP-QTRAOHFR>
2024-10-20 16:06:54 +0700

    first commit

--- hotel_booking_demo/src/main/java/com/vhung11/hotel_booking_demo/controller/UserController.java
+++ hotel_booking_demo/src/main/java/com/vhung11/hotel_booking_demo/controller/UserController.java
@@ -10,11 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/users")
     public User createUser(@RequestBody UserCreationRequest request) {
