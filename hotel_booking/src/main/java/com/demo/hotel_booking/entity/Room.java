73d5c8d0b1deb1ef16c4fe3bde90137706fd724b
Bui Van Huy <buivanhuy18102003@gmail.com>
2024-10-28 11:59:11 +0700

    feat: add room (add room entity)

---
+++ hotel_booking/src/main/java/com/demo/hotel_booking/entity/Room.java
@@ -0,0 +1,16 @@
+package com.demo.hotel_booking.entity;
+
+public class Room {
+    private String hotelID;
+    private String roomID;
+    private String name;
+    private String description;
+    private String type;
+    private String status;
+    private String price;
+    private String capacity;
+    private String image;
+    private String createdAt;
+    private String updatedAt;
+    private String deletedAt;
+}
