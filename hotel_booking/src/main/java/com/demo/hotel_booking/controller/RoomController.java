c30d1825a5f526fdf47159396c187ea89a6762fe
Bui Van Huy <buivanhuy18102003@gmail.com>
2024-11-16 16:57:01 +0700

    feat: add room (update RoomController)

--- hotel_booking/src/main/java/com/demo/hotel_booking/controller/RoomController.java
+++ hotel_booking/src/main/java/com/demo/hotel_booking/controller/RoomController.java
@@ -2,6 +2,7 @@ package com.demo.hotel_booking.controller;
 
 import com.demo.hotel_booking.entity.Room;
 import com.demo.hotel_booking.service.RoomService;
+import com.demo.hotel_booking.service.RoomServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/rooms")
+@RequestMapping("/managers")
 public class RoomController {
     @Autowired
-    private RoomService roomService;
+    private RoomServiceImpl roomService;
 
-    @PostMapping
+    @PostMapping("/add_rooms")
     public ResponseEntity<Room> createRoom(@RequestBody Room room) {
         Room createdRoom = roomService.createRoom(room);
         return ResponseEntity.ok(createdRoom);
