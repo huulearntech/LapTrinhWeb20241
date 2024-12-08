578a31c396cb254addd8ad7b72cc405c59321bb9
Bui Van Huy <buivanhuy18102003@gmail.com>
2024-12-08 20:34:54 +0700

    delete user file

--- hotel_booking/src/main/java/com/demo/hotel_booking/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.hotel_booking.repository;
-
-import com.demo.hotel_booking.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
